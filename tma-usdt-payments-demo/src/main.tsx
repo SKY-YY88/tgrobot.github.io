f7f2915704b22a227cb3b9989413dbfce8f19ade
SKY-YY88 <70370764+SKY-YY88@users.noreply.github.com>
2024-11-12 11:28:54 +0800

    Delete tma-usdt-payments-demo directory

--- tma-usdt-payments-demo/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import './polyfill';
-
-import ReactDOM from 'react-dom/client';
-
-import { Root } from '@/components/Root.tsx';
-
-import '@/assets/styles/globals.scss';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<Root/>);
