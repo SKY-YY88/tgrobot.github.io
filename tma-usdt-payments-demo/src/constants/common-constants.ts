7a6543333a483e8898d75bac12f4ae0ebcfef231
SKY-YY88 <70370764+SKY-YY88@users.noreply.github.com>
2024-11-12 10:59:32 +0800

    Add files via upload

---
+++ tma-usdt-payments-demo/src/constants/common-constants.ts
@@ -0,0 +1,7 @@
+import { Address } from '@ton/core';
+
+// export const USDT_MASTER_ADDRESS = Address.parse('kQD0GKBM8ZbryVk2aESmzfU6b9b_8era_IkvBSELujFZPsyy');
+// export const INVOICE_WALLET_ADDRESS = Address.parse('0QD-SuoCHsCL2pIZfE8IAKsjc0aDpDUQAoo-ALHl2mje04A-');
+
+export const USDT_MASTER_ADDRESS = Address.parse('EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs');
+export const INVOICE_WALLET_ADDRESS = Address.parse('UQB1gfLjqXQ8rAtocM8xU36hDIp9cg3HUTvXen9K3S9qnHXa');
---
+++ tma-usdt-payments-demo/src/hooks/useTonConnect.ts
@@ -0,0 +1,48 @@
+import { useContext } from 'react';
+import { TonClient } from '@ton/ton';
+import { CHAIN, useTonConnectUI, useTonWallet } from '@tonconnect/ui-react';
+import { Address, Sender, SenderArguments } from '@ton/core';
+import { TonConnectUI } from '@tonconnect/ui';
+import { TonClientContext } from '@/context/ton-client-context.tsx';
+
+
+export const useTonConnect = (): {
+  sender: Sender;
+  connected: boolean;
+  walletAddress: Address | null;
+  network: CHAIN | null;
+  tonConnectUI: TonConnectUI;
+  tonClient: TonClient | undefined;
+} => {
+  const [tonConnectUI] = useTonConnectUI();
+  const wallet = useTonWallet();
+  // alert(wallet?.account?.chain)
+  // alert(wallet?.account?.address)
+  const { tonClient } = useContext(TonClientContext);
+
+  const walletAddress = wallet?.account?.address ? Address.parse(wallet.account.address) : undefined;
+  return {
+    // 将交易发送到 TON 区块链
+    sender: {
+      send: async (args: SenderArguments) => {
+        await tonConnectUI.sendTransaction({
+          messages: [
+            {
+              address: args.to.toString(),
+              amount: args.value.toString(),
+              payload: args.body?.toBoc()?.toString('base64'),
+            },
+          ],
+          validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes for user to approve
+        });
+      },
+      address: walletAddress,
+    },
+    // 判断钱包是否已连接
+    connected: !!wallet?.account?.address,
+    walletAddress: walletAddress ?? null,
+    network: wallet?.account?.chain ?? null,
+    tonConnectUI,
+    tonClient
+  };
+};
